d7d2559e0fc331ed0da1b75fe4f3854e9b21225f
yprajapati-007 <yogesh.kumar@bughunters.io>
2023-08-02 15:35:15 +0530

    added config file and gitIgnore file

---
+++ playwright.config.js
@@ -0,0 +1,40 @@
+// @ts-check
+const { defineConfig, devices } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  timeout: 600 * 1000,
+  expect: {
+    timeout: 600000
+  },
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'html',
+  use: {
+    actionTimeout: 0,
+    trace: 'on-first-retry',
+    // Enable video recording for the 'chromium' project
+    video: 'on',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+        launchOptions: {
+          headless: false, // Run the browser in non-headless mode
+        },
+        // Enable video recording for the 'chromium' project
+        video: 'on',
+      },
+    },
+    // Other projects can be defined here...
+
+  ],
+
+  // Other configurations can be added here...
+
+});
